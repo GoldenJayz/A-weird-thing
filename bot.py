c707abb6f04e866f861f3c99c4129b4d0974be4a
GoldenJayz <45317852+GoldenJayz@users.noreply.github.com>
2019-11-04 15:45:38 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ Clientdiscord = discord.Client()
 
 @client.event
 async def on_ready():
-    await client.change_presence(game=Game(name='V7 :)', type = 3))
+    await client.change_presence(game=Game(name='Beta Testing Version 8 :)', type = 3))
     print('Loading AlteriaALPHA V3.1.2, ... Loaded Successfully.')
     
 @client.event

2d6fefeba2b722c8901a5e83809c25b439cf9787
GoldenJayz <45317852+GoldenJayz@users.noreply.github.com>
2020-05-25 13:57:50 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ client = commands.Bot(command_prefix = '-')
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game('Type -commands for commands!'), status=discord.Status.online)
+    await client.change_presence(activity=discord.Game('Testing version'), status=discord.Status.online)
     print('We have logged in as {0.user}'.format(client))
 
 
@@ -71,10 +71,9 @@ async def level_up(users, user, channel):
     
 #Commands
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('-commands'):
-        em = discord.Embed( 
+@client.command
+async def commands(ctx):
+    em = discord.Embed( 
             title='Commands',
             description='A simple list of commands',
             colour = discord.Colour.orange()
@@ -89,166 +88,5 @@ async def on_message(message):
 
         await message.channel.send('', embed=em)
 
-    if message.content.startswith('-nezukogif'):
-        randomgif=['https://media.giphy.com/media/ghBwyDXSAEto8PztXc/giphy.gif', 'https://media1.tenor.com/images/08aea234985337f204997fdba0cfce38/tenor.gif?itemid=15688586', 'https://media1.giphy.com/media/dAu9Hz9xEDMPCw6Lwz/source.gif',]
-        em = discord.Embed(
-            title='Nezuko GIF executed.',
-            colour = discord.Colour.purple()
-        )
-        em.set_image(url= (random.choice(randomgif)))
-        await message.channel.send(embed=em)
-
-    if message.content.startswith('-nezukorunningmeme'):
-        em = discord.Embed(
-            description='**Sonic the Hedgehog music starts playing**',
-            colour = discord.Colour.purple()
-            )
-        em.set_image(url='https://media.giphy.com/media/UTSDPWVW4Llw1rPC9k/giphy.gif')
-        await message.channel.send('', embed=em)
-
-    if message.content.startswith('-menu'):
-        em = discord.Embed(
-            title='Menu',
-            description='Menu to order food from :).',
-            colour = discord.Colour.blue()
-        )
-        em.add_field(name='**McDonalds Food:**', value='Big Mac\nChicken McNuggets\nFrench Fries', inline=True)
-        em.add_field(name='**Beverages:**', value='Sprite\nCoca-Cola\nPepsi\nRoot Beer', inline=True)
-        em.add_field(name='**KFC Food:**', value='Chicken Bucket\nChicken Tenders', inline=True)
-        em.set_footer(text='More will be coming soon...')
-        await message.channel.send('', embed=em)
-
-    if message.content.startswith('-big mac'):
-        em = discord.Embed(
-            title='Big Mac',
-            description='Your order has came through :)',
-            colour = discord.Colour.red()
-        )
-        em.set_image(url='https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/hero/desktop/t-mcdonalds-Big-Mac.jpg?$Product_Desktop$')
-        await message.channel.send('', embed=em)
-
-    if message.content.startswith('-chicken mcnuggets'):
-        em = discord.Embed(
-            title='Chicken McNuggets',
-            description='Your order has came through :)',
-            colour = discord.Colour.red()
-        )
-        em.set_image(url='https://upload.wikimedia.org/wikipedia/commons/d/d0/McDonalds-Chicken-McNuggets.jpg')
-        await message.channel.send('', embed=em)
-
-    if message.content.startswith('-french fries'):
-        em = discord.Embed(
-            title='French Fries',
-            description='Your order has came through :)',
-            colour = discord.Colour.red()
-        )
-        em.set_image(url='https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/hero/desktop/t-mcdonalds-Fries-Small-Medium.jpg?$Product_Desktop$')
-        await message.channel.send('', embed=em)
-
-    if message.content.startswith('-sprite'):
-        em = discord.Embed(
-            title='Sprite',
-            description='Your order has came through :)',
-            colour = discord.Colour.red()
-        )
-        em.set_image(url='https://images-na.ssl-images-amazon.com/images/I/71Y5MLLmknL._SL1500_.jpg')
-        await message.channel.send('', embed=em)
-
-    if message.content.startswith('-cocacola'):
-        em = discord.Embed(
-            title='Coca-Cola',
-            description='Your order has came through :)',
-            colour = discord.Colour.red()
-        )
-        em.set_image(url='https://images-na.ssl-images-amazon.com/images/I/71W%2BHikjmKL._SL1500_.jpg')
-        await message.channel.send('', embed=em)
-
-    if message.content.startswith('-pepsi'):
-        em = discord.Embed(
-            title='Pepsi',
-            description='Your order has came through :)',
-            colour = discord.Colour.red()
-        )
-        em.set_image(url='https://www.pepsi.com/en-us/uploads/images/social-share.jpg?mtime=20180110134930')
-        await message.channel.send('', embed=em)
-
-    if message.content.startswith('-rootbeer'):
-        em = discord.Embed(
-            title='Root Beer',
-            description='Your order has came through :)',
-            colour = discord.Colour.red()
-        )
-        em.set_image(url='https://upload.wikimedia.org/wikipedia/commons/7/7d/Root_beer_in_glass_mug.jpg')
-        await message.channel.send('', embed=em)
-
-    if message.content.startswith('-chickenbucket'):
-        em = discord.Embed(
-            title='Chicken Bucket',
-            description='Your order has came through :)',
-            colour = discord.Colour.red()
-        )
-        em.set_image(url='https://www.thepackagingcompany.us/knowledge-sharing/wp-content/uploads/2018/09/ip-kfcbucket-blog.jpg')
-        await message.channel.send('', embed=em)
-
-    if message.content.startswith('-chickentenders'):
-        em = discord.Embed(
-            title='Chicken Tenders',
-            description='Your order has came through :)',
-            colour = discord.Colour.red()
-        )
-        em.set_image(url='https://www.dinneratthezoo.com/wp-content/uploads/2015/04/chicken-fingers-4.jpg')
-        await message.channel.send('', embed=em)
-
-    if message.content.startswith('-test'):
-        em = discord.Embed(
-            title = 'commands',
-            description = 'Testing',
-            colour = discord.Colour.blue()
-        )
-        await message.channel.send('', embed=em)
-
-    if message.content.startswith('-8ball'):
-        randomlist = ['Maybe','Yes','No',]
-        randomgif = ['https://media3.giphy.com/media/141iprzbEPjCiQ/giphy.gif', 'https://i.imgur.com/akdtE4H.gif', 'https://thumbs.gfycat.com/QuestionableMajesticBillygoat-small.gif',]
-        em = discord.Embed(
-            title="The magic 8 ball has spoken.",
-            description=(random.choice(randomlist)),
-            colour = discord.Colour.blurple()
-        )
-        em.set_image(url= (random.choice(randomgif)))
-        await message.channel.send(embed=em)
-    
-    if message.content.startswith('-jojo'):
-        randomlist = ['https://media.giphy.com/media/h7FZESJ1Ter5bGhUDG/giphy.gif',  'https://thumbs.gfycat.com/ForthrightBoldDaddylonglegs-max-1mb.gif', 'https://media.giphy.com/media/f9jxYYRVPHtKsCf9sy/giphy.gif']
-        em = discord.Embed(
-            title='Here is your Jojo gif.',
-            description=':)',
-            colour = discord.Colour.purple()
-        )
-        em.set_image(url= (random.choice(randomlist)))
-        await message.channel.send(embed=em)
-
-    if message.content.startswith('-clear'):
-        amount=5
-        await message.channel.purge(limit=amount)
-
-    if message.content.startswith('-shoot'):
-        em = discord.Embed(
-            title='You have been shot!',
-            description='You have a gun shot in your chest',
-            colour = discord.Colour.green()
-        )
-        em.set_image(url='https://media.giphy.com/media/9umH7yTO8gLYY/giphy.gif')
-        await message.channel.send('', embed=em)
-     
-    if message.content.startswith('-demonslayer'):
-        randomlist = ['https://media.giphy.com/media/VEzYdo930nTiTuVeMU/giphy.gif',  'https://media.giphy.com/media/J6JazAkCVLId91L4yM/giphy.gif', 'https://media.giphy.com/media/Yq1tXTNe5PDdQEhKXG/giphy.gif', 'https://media.giphy.com/media/TgyJebqyMtPrOxiPdk/giphy.gif', 'https://media.giphy.com/media/f7k6TfAFkiAqKVcJGH/giphy.gif', 'https://media.giphy.com/media/JrHB5IyuBzKwPEPZe4/giphy.gif', 'https://media.giphy.com/media/H83c2x0NWvFhtgK4V2/giphy.gif', 'https://media.giphy.com/media/ZaueN0ipnurQlgKsRu/giphy.gif', 'https://media.giphy.com/media/dVcrkGH9DU9EpdGsV8/giphy.gif']
-        em = discord.Embed(
-            title='Demon Slayer Gif!',
-            description=':)',
-            colour = discord.Colour.purple()
-        )
-        em.set_image(url= (random.choice(randomlist)))
-        await message.channel.send(embed=em)
 client.run(str(os.environ.get('BOT_TOKEN')))
 
